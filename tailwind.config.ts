dfd29e7473094a24291016eef2fc2bc9cf9eec8f
Kazem Kamali <itskazemk@gmail.com>
2024-07-24 19:42:27 +0330

    prisma

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,7 +6,10 @@ const config: Config = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-
   plugins: [require("@tailwindcss/typography"), require("daisyui")],
+
+  daisyui: {
+    themes: ["light"],
+  },
 };
 export default config;
