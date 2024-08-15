52458a037494e19921058239329c2b030ff581aa
Kazem <itskazemk@gmail.com>
2024-08-15 18:41:31 +0330

    added role to user model

--- prisma/migrations/20240815151050_init/migration.sql
+++ prisma/migrations/20240815151050_init/migration.sql
@@ -53,6 +53,30 @@ CREATE TABLE "Connection" (
     CONSTRAINT "Connection_pkey" PRIMARY KEY ("id")
 );
 
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "userName" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "role" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Sessions" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "expiresAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Sessions_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_userName_key" ON "User"("userName");
+
 -- AddForeignKey
 ALTER TABLE "Connection" ADD CONSTRAINT "Connection_primaryDriverId_fkey" FOREIGN KEY ("primaryDriverId") REFERENCES "Driver"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
@@ -64,3 +88,6 @@ ALTER TABLE "Connection" ADD CONSTRAINT "Connection_vehicleId_fkey" FOREIGN KEY 
 
 -- AddForeignKey
 ALTER TABLE "Connection" ADD CONSTRAINT "Connection_routeId_fkey" FOREIGN KEY ("routeId") REFERENCES "Route"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Sessions" ADD CONSTRAINT "Sessions_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
