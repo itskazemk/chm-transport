a83752e23649a233f504106181aa87d08aed8af2
Kazem Kamali <itskazemk@gmail.com>
2024-09-08 18:40:31 +0330

    commented footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import type { Metadata } from "next";
 import Link from "next/link";
 import "iran-license-plate/dist/License.css";
 import "./globals.css";
-import Footer from "@/components/Footer";
+// import Footer from "@/components/Footer";
 import localFont from "next/font/local";
 import "@mantine/core/styles.css";
 import { ColorSchemeScript } from "@mantine/core";
@@ -33,7 +33,7 @@ export default function RootLayout({
         <Providers>
           <main className="flex h-screen flex-col">
             <div className="flex-1">{children}</div>
-            <Footer />
+            {/* <Footer /> */}
           </main>
         </Providers>
       </body>
